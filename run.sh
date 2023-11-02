87f0c7715dc05a07e3dabf710ff29d28dc804300
felipemindlin <fmindlin@itba.edu.ar>
2023-11-02 15:52:10 -0300

    seteo de gdb

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 2048
+if [[ "$1" = "gdb" ]]; then
+    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 2048 -s -S
+else
+    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 2048
+fi
