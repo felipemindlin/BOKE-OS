324905f26c9eec5201f30af4d9dba842543834f3
tbussozungri <tbussozungri@itba.edu.ar>
2023-11-06 19:20:04 -0300

    Pipes:first version implemented

---
+++ Kernel/include/pipe.h
@@ -0,0 +1,29 @@
+#include <stdint.h>
+
+#define PIPE_BUFFER_SIZE 1024
+#define MAX_PIPES 10
+#define EOF -1
+typedef struct pipe{
+    int name;
+    int readSemId;
+    int writeSemId;
+    unsigned int writePos;
+    unsigned int readPos;
+    char *pipeBuffer;
+    //uint8_t eof;
+    //int leftToRead;
+} pipe;
+
+void init_pipes();
+
+int create_pipe(int name);
+
+int pipe_write(int id, const char *message, unsigned int size);
+
+int pipe_read(int id, char * dest, unsigned int size);
+
+void close_pipe(int id);
+
+int getPipeID(int name);
+
+pipe getPipe(int id);
